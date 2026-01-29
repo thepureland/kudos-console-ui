792c48cf4b759a4ee605ae9c62c6c50e6c37026e
kevice <kevice@mail.com>
2026-01-29 23:07:23 +0800

    chore: delete useless files

--- webApp/src/App.vue
+++ webApp/src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <AdminHome v-if="hasToken" />
+  <Home v-if="hasToken" />
   <Login v-else />
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue';
 import Login from './components/Login/Login.vue';
-import AdminHome from './pages/AdminHome.vue';
+import Home from './pages/Home.vue';
 import { AuthApiFactory } from 'shared';
 
 const hasToken = computed(() => AuthApiFactory.getInstance().hasToken());
